66d56cf85c89026766fa0110d23679b114888fdb
juns <141826785+junexi0828@users.noreply.github.com>
2024-05-01 22:52:38 +0900

    Add files via upload

--- lecture4/src/main/java/form/handle/ProcessRequest.java
+++ lecture4/src/main/java/form/handle/ProcessRequest.java
@@ -1,64 +1,64 @@
-//package form.handle;
-//
-//import jakarta.servlet.ServletException;
-////import jakarta.servlet.annotation.WebServlet;
-//import jakarta.servlet.http.HttpServlet;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//import java.io.PrintWriter;
-//import java.util.Enumeration;
-//
-///**
-// * Servlet implementation class ProcessRequest
-// */
-//public class ProcessRequest extends HttpServlet {
-//	private static final long serialVersionUID = 1L;
-//       
-//    /**
-//     * @see HttpServlet#HttpServlet()
-//     */
-//    public ProcessRequest() {
-//        super();
-//        // TODO Auto-generated constructor stub
-//    }
-//
-//	/**
-//	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-//	 */
-//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//	   Enumeration<String> params = request.getParameterNames();
-//	   response.setContentType("text/html");
-//	   PrintWriter pw = response.getWriter();
-//	   pw.println("<html><body>");
-//	   pw.println("I've received these parameters:<br><br>");
-//	   
-//	   while(params.hasMoreElements()) {
-//	      String param = params.nextElement();
-//	      if ("reason".equals(param)) {
-//	         String[] values = request.getParameterValues(param);
-//	         for (String v: values) {
-//	            pw.append(param + ": " + v + "<br>");
-//	         }
-//	      } else {
-//	         String value = request.getParameter(param);
-//	         pw.append(param + ": " + value + "<br>");
-//	      }
-//	   }
-//	   pw.println("</body></html>");
-//	   pw.close();		
-//	}
-//
-//	/**
-//	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-//	 * post를 해줘야 웹에서 다시 입력값을 보여줄수있음 그래서 다시 post로 doget을 불러와주는 과정  
-//	 */
-//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//			// TODO Auto-generated method stub
-//			doGet(request, response);
-//	
-//	//	System.out.println("Handled in doPost().");
-//	}
-//
-//}
-//
+package form.handle;
+
+import jakarta.servlet.ServletException;
+//import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+/**
+ * Servlet implementation class ProcessRequest
+ */
+public class ProcessRequest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ProcessRequest() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	   Enumeration<String> params = request.getParameterNames();
+	   response.setContentType("text/html");
+	   PrintWriter pw = response.getWriter();
+	   pw.println("<html><body>");
+	   pw.println("I've received these parameters:<br><br>");
+	   
+	   while(params.hasMoreElements()) {
+	      String param = params.nextElement();
+	      if ("reason".equals(param)) {
+	         String[] values = request.getParameterValues(param);
+	         for (String v: values) {
+	            pw.append(param + ": " + v + "<br>");
+	         }
+	      } else {
+	         String value = request.getParameter(param);
+	         pw.append(param + ": " + value + "<br>");
+	      }
+	   }
+	   pw.println("</body></html>");
+	   pw.close();		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * post를 해줘야 웹에서 다시 입력값을 보여줄수있음 그래서 다시 post로 doget을 불러와주는 과정  
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			// TODO Auto-generated method stub
+			doGet(request, response);
+	
+	//	System.out.println("Handled in doPost().");
+	}
+
+}
+
